58a398ed64be3e2ea1dd4eedeade9a4ca1c35112
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2024-10-15 18:26:51 +0300

    Add postgres config

---
+++ src/config/database.py
@@ -0,0 +1,24 @@
+from os import environ
+
+from dotenv import load_dotenv
+from sqlalchemy import URL
+from sqlmodel import Session, SQLModel, create_engine
+
+load_dotenv()
+
+
+url_object = URL.create(
+    drivername="postgresql",
+    username=environ["POSTGRES_USER"],
+    password=environ["POSTGRES_PASSWORD"],
+    host=environ["POSTGRES_HOST"],
+    port=environ["POSTGRES_PORT"],
+    database=environ["POSTGRES_DATABASE"],
+)
+engine = create_engine(url_object, echo=True)
+SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
