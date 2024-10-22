99f733d011c141d80c586524df013c6d99949795
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2024-10-22 16:42:26 +0300

    Add get user method

--- src/users/routers.py
+++ src/users/routers.py
@@ -41,14 +41,8 @@ async def register(payload: User, session=Depends(get_session)):
         )
 
 
-@router.get("/users/", tags=["users"], response_model=list[UserResponse])
-async def users(session=Depends(get_session), auth=Depends(oauth2_scheme)):
-    statement = select(User)
-    return session.exec(statement).all()
-
-
 @router.post("/auth/token", tags=["users"], response_model=Token)
-async def get_access_token(
+async def login(
     form_data=Depends(OAuth2PasswordRequestForm), session=Depends(get_session)
 ):
     user = authenticate(session, form_data.username, form_data.password)
@@ -62,3 +56,20 @@ async def get_access_token(
         "access_token": create_access_token(data={"sub": user.username}),
         "token_type": "bearer",
     }
+
+
+@router.get("/users/", tags=["users"], response_model=list[UserResponse])
+async def get_all_users(session=Depends(get_session), auth=Depends(oauth2_scheme)):
+    statement = select(User)
+    return session.exec(statement).all()
+
+
+@router.get("/users/{id}/", tags=["users"], response_model=UserResponse)
+async def get_user_by_id(id, session=Depends(get_session)):
+    try:
+        statement = select(User).where(User.id == id)
+        return session.exec(statement).one()
+    except SQLAlchemyError as exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=str(exception)
+        )
