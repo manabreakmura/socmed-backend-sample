72260374f5da9122e9a202242a11d71cf951bb7c
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2025-06-16 13:46:39 +0300

    Add db config

---
+++ src/config/db.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from decouple import config
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlmodel import SQLModel
+
+engine = create_async_engine(config("DATABASE_URL"), echo=True)
+session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with session() as se:
+        yield se
+
+
+async def create_db_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+session_dep = Annotated[AsyncSession, Depends(get_session)]
--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from config.db import create_db_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
