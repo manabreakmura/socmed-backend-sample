61e1099aa5be3b10c99d147b8c497c544ce88f8a
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2026-02-18 17:18:54 +0000

    Add cookie auth

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,9 +1,15 @@
 from decouple import config
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, HTTPException, Request, Response, status
 from sqlalchemy.exc import IntegrityError
 from sqlmodel import select
 
-from config.auth import encode_access_token, form_data, hash_password, verify_password
+from config.auth import (
+    decode_access_token,
+    encode_access_token,
+    form_data,
+    hash_password,
+    verify_password,
+)
 from config.db import session_dep
 from config.log import logger
 from users.models import User
@@ -58,6 +64,44 @@ async def login(session: session_dep, form_data: form_data, response: Response) 
         return access_token
     except HTTPException:
         raise
+    except Exception as e:
+        logger.error(f"ERROR: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@auth_router.get("/me")
+async def get_current_auth(session: session_dep, request: Request):
+    try:
+        access_token = request.cookies.get("access_token")
+
+        if not access_token:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+        user_id = decode_access_token(access_token)
+
+        statement = select(User).where(User.id == user_id)
+        results = await session.execute(statement)
+        row = results.scalar()
+
+        if not row:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+        return {"id": row.id, "email": row.email, "username": row.username}
+
+    except Exception as e:
+        logger.error(f"ERROR: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@auth_router.post("/logout")
+async def logout(response: Response):
+    try:
+        response.delete_cookie(
+            key="access_token",
+            httponly=True,
+            samesite="lax",
+            secure=False if config("DEBUG", cast=bool) else True,
+        )
     except Exception as e:
         logger.error(f"ERROR: {e}")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
